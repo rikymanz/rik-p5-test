c12d549aa5a7c152262245e0f7de63a3eabea1b1
rikymanz <31204509+rikymanz@users.noreply.github.com>
2023-03-29 15:21:33 +0200

    Update test.js
    Test curve

--- test.js
+++ test.js
@@ -1,16 +1,12 @@
-
-let circle_1 = { x:400 , y:400 }
-let circle_2 = { x:405 , y:400 }
-let circle_3 = { x:400 , y:405 }
-let circle_4 = { x:405 , y:405 }
-
-let array = [
+let arr = [
     
-    { x:400 , y:400 },
-    { x:405 , y:400 },
-    { x:400 , y:405 },
-    { x:405 , y:405 },
+    { x:400 , y:400 , color:0},
+    { x:405 , y:400 , color:80 },
+    { x:400 , y:405 , color:160},
+    { x:405 , y:405 , color:240},
 ]
+
+let x = 0
 
 
 function setup() {
@@ -20,13 +16,17 @@ function setup() {
 
 function draw() {
 
-    for( let i = 0 ; i < array.length ; i++){
-        let c = array[i]
+    for( let i = 0 ; i < arr.length ; i++){
+        let c = arr[i]
+        fill( c.color )
         circle( c.x , c.y , 50 )
 
-        c.x = c.x + 0.5;
-        c.y = c.y + 0.25;
+        c.x = i > 1 ? c.x + x : c.x - x
+        c.y = i % 2 === 0 ? c.y + x * x : c.y - x * x 
+      
+        
     }
+  
+    x = x + 0.1
 
 }
-    //END
